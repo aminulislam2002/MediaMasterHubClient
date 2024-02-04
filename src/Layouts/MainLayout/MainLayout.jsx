c70abd88f3b95e4f68164ccbefaf707451f97d94
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-02-04 19:53:35 +0600

    install tailwind css and setup

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../../Pages/SharedPages/NavBar/NavBar";
+import Footer from "../../Pages/SharedPages/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
